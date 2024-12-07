c77bd8ba91b4e02e48a2962d8c0155904f3b676f
haotianzhang124 <haotianzhang124@gmail.com>
2024-12-07 23:03:42 +0800

    no message

--- mytest/testGame/Classes/AppDelegate.cpp
+++ mytest/testGame/Classes/AppDelegate.cpp
@@ -1,30 +1,6 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
-
+#include "proj.win32/constant.h"
 // #define USE_AUDIO_ENGINE 1
 // #define USE_SIMPLE_AUDIO_ENGINE 1
 
@@ -42,16 +18,16 @@ using namespace CocosDenshion;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+static cocos2d::Size designResolutionSize = cocos2d::Size(DESIGN_RESOLUTION_WIDTH, DESIGN_RESOLUTION_HEIGHT);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(SMALL_RESOLUTION_WIDTH, SMALL_RESOLUTION_HIGHT);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(NEDIUM_RESOLUTION_WIDTH, NEDIUM_RESOLUTION_HEIGHT);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(LARGE_RESOLUTION_WIDTH, LARGE_RESOLUTION_HEIGHT);
 
 AppDelegate::AppDelegate()
 {
 }
 
-AppDelegate::~AppDelegate() 
+AppDelegate::~AppDelegate()
 {
 #if USE_AUDIO_ENGINE
     AudioEngine::end();
@@ -65,7 +41,7 @@ AppDelegate::~AppDelegate()
 void AppDelegate::initGLContextAttrs()
 {
     // set OpenGL context attributes: red,green,blue,alpha,depth,stencil,multisamplesCount
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8, 0};
+    GLContextAttrs glContextAttrs = { 8, 8, 8, 8, 24, 8, 0 };
 
     GLView::setGLContextAttrs(glContextAttrs);
 }
@@ -76,22 +52,23 @@ static int register_all_packages()
 {
     return 0; //flag for packages manager
 }
-
+// 当应用程序完成启动并准备进入主循环时调用此函数
 bool AppDelegate::applicationDidFinishLaunching() {
     // initialize director
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
-    if(!glview) {
+    if (!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("testGame", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        //修改程序名称和分辨率
+        glview = GLViewImpl::createWithRect(NameOfGame, cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview->setDesignResolutionSize(960, 640, ResolutionPolicy::SHOW_ALL);
 #else
-        glview = GLViewImpl::create("testGame");
+        glview = GLViewImpl::create("MyGame");
 #endif
         director->setOpenGLView(glview);
     }
-
     // turn on display FPS
-    director->setDisplayStats(true);
+    director->setDisplayStats(false);
 
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0f / 60);
@@ -101,26 +78,27 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto frameSize = glview->getFrameSize();
     // if the frame's height is larger than the height of medium size.
     if (frameSize.height > mediumResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
+    {
+        director->setContentScaleFactor(MIN(largeResolutionSize.height / designResolutionSize.height, largeResolutionSize.width / designResolutionSize.width));
     }
     // if the frame's height is larger than the height of small size.
     else if (frameSize.height > smallResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
+    {
+        director->setContentScaleFactor(MIN(mediumResolutionSize.height / designResolutionSize.height, mediumResolutionSize.width / designResolutionSize.width));
     }
     // if the frame's height is smaller than the height of medium size.
     else
-    {        
-        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
+    {
+        director->setContentScaleFactor(MIN(smallResolutionSize.height / designResolutionSize.height, smallResolutionSize.width / designResolutionSize.width));
     }
 
     register_all_packages();
 
-    // create a scene. it's an autorelease object
+    // 修改下面这两行函数
+    //创建helloworld的场景
     auto scene = HelloWorld::createScene();
 
-    // run
+    // 运行场景
     director->runWithScene(scene);
 
     return true;
--- mytest/testGame/Classes/HelloWorldScene.cpp
+++ mytest/testGame/Classes/HelloWorldScene.cpp
@@ -1,32 +1,10 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
 #include "HelloWorldScene.h"
 #include "SimpleAudioEngine.h"
-
+#include "proj.win32/constant.h"
+#include "ui/CocosGUI.h"
+//#include "constant.h"
 USING_NS_CC;
-
+std::string playerName = "";
 Scene* HelloWorld::createScene()
 {
     return HelloWorld::create();
@@ -42,79 +20,107 @@ static void problemLoading(const char* filename)
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Scene::init() )
+    //初始化失败返回false
+    if (!Scene::init())
     {
         return false;
     }
 
+
+    //创建背景
+    //获取游戏的窗口大小
     auto visibleSize = Director::getInstance()->getVisibleSize();
+
+
+    //获取屏幕的原点位置，即左下角坐标，通常是（0,0）
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    auto background = Sprite::create("StartScene.jpg");
+    Size originalSize = background->getContentSize(); // 图片的原始大小
+    Size targetSize = Size(DESIGN_RESOLUTION_WIDTH, DESIGN_RESOLUTION_HEIGHT); // 目标大小
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+    // 计算缩放比例
+    float scaleX = targetSize.width / originalSize.width;
+    float scaleY = targetSize.height / originalSize.height;
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+    // 设置缩放比例
+    background->setScale(scaleX, scaleY);
+    background->setPosition(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2);
 
-    if (closeItem == nullptr ||
-        closeItem->getContentSize().width <= 0 ||
-        closeItem->getContentSize().height <= 0)
-    {
-        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
-    }
-    else
-    {
-        float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
-        float y = origin.y + closeItem->getContentSize().height/2;
-        closeItem->setPosition(Vec2(x,y));
-    }
+    this->addChild(background, 0);
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else
-    {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
-
-        // add the label as a child to this layer
-        this->addChild(label, 1);
-    }
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+    //添加艺术字
+    auto sprite = Sprite::create("begin.png");
     if (sprite == nullptr)
     {
-        problemLoading("'HelloWorld.png'");
+        problemLoading("begin.png");
     }
     else
     {
+        sprite->setScale(0.75);
         // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+        sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height * 3 / 4 + origin.y));
 
         // add the sprite as a child to this layer
         this->addChild(sprite, 0);
     }
+
+    // 创建文本框
+    auto textField = cocos2d::ui::TextField::create(u8"请输入您的游戏昵称", "../Resources/fonts/DingDingJinBuTi.ttf", TEXT_SIZE);
+    textField->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
+    textField->setMaxLength(6);
+    textField->setMaxLengthEnabled(true);
+    textField->setTextColor(cocos2d::Color4B(0, 32, 96, 255));
+    this->addChild(textField);
+    // 创建标签
+    auto promptLabel = Label::createWithTTF("", "../Resources/Fonts/DingDingJinBuTi.ttf", TEXT_SIZE);
+    promptLabel->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 - 50));
+    promptLabel->setTextColor(cocos2d::Color4B(COLOR_RED, COLOR_GREEN, COLOR_BLUE, ALPHA));
+    this->addChild(promptLabel);
+
+    // 为文本框添加事件监听器
+    textField->addEventListener([promptLabel](Ref* sender, cocos2d::ui::TextField::EventType type) {
+        // 使用 Lambda 表达式为文本框添加事件监听器。Lambda 表达式捕获了外部的 promptLabel 对象，这样可以在事件处理中修改这个标签的内容。
+        // Lambda 表达式会处理两种事件类型：文本插入（INSERT_TEXT）和文本删除（DELETE_BACKWARD）。
+
+        if (type == cocos2d::ui::TextField::EventType::INSERT_TEXT || type == cocos2d::ui::TextField::EventType::DELETE_BACKWARD) {
+            auto textField = dynamic_cast<cocos2d::ui::TextField*>(sender);
+            std::string nickname = textField->getString();
+            std::string text = u8"欢迎来到星露谷物语！" + nickname;
+            promptLabel->setString(text);
+        }
+        });
+
+    //创建按钮
+    auto startButton = ui::Button::create("CreateANewRole.png", "CreateANewRoleSelect.png");
+    float stratButtonX = targetSize.width / originalSize.width;
+    float stratButtonY = targetSize.height / originalSize.height;
+
+    // 设置缩放比例和位置
+    startButton->setScale(stratButtonX, stratButtonY);
+    startButton->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 5));
+    this->addChild(startButton);
+
+    // 创建标签
+    auto nameLabel = Label::createWithTTF("", "../Resources/Fonts/DingDingJinBuTi.ttf", TEXT_SIZE);
+    nameLabel->setPosition(stratButtonX, stratButtonY + 50);
+    nameLabel->setVisible(false);
+    nameLabel->setTextColor(cocos2d::Color4B(COLOR_RED, COLOR_GREEN, COLOR_BLUE, ALPHA));
+
+    //// 为按钮添加事件
+    //startButton->addClickEventListener([this,nameLabel,textField](Ref* sender) {
+    //    std::string nickname = textField->getString();
+    //    if (nickname.empty()) {
+    //        nameLabel->setString(u8"游戏昵称不能为空");
+    //        nameLabel->setVisible(true);
+    //        this->scheduleOnce([nameLabel](float dt) {
+    //            nameLabel->setVisible(false);
+    //            }, 1.5, "HideEmptyPromptLabel");
+    //    }
+    //    else {
+    //        playerName = nickname;
+    //        cocos2d::Director::getInstance()->replaceScene(cocos2d::TransitionFade::create(0.3, SelectionScene::createScene(), cocos2d::Color3B::WHITE));
+    //    }
+    //    });
     return true;
 }
 
